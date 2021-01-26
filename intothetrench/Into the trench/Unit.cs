2bed40266865ee8f8f2f7ccdc1e144c316b038bf
Kevin Nunes da Silva <kmh.nunesdasilva@gmail.com>
2021-01-26 01:56:35 +0100

    layout

---
+++ intothetrench/Into the trench/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Into_the_trench
+{
+    class Unit
+    {
+        private int _id;
+        private int _level;
+        private string _type;
+        private string _name;
+        private int _hp;
+        private int _attack;
+
+    }
+}
